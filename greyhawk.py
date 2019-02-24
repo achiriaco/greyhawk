8bb5f245be9426a8465276da0a09e0c617a9f711
Angelo Chiriaco <achiriaco@hotmail.com>
2019-02-23 21:46:48 -0800

    WIP / adding  json and python

--- greyhawk.py
+++ greyhawk.py
@@ -1,12 +1,29 @@
-Input_Month = "Readying"
-Input_Day = "5"
+# Program steps
+# 1. Input of what Month
+# 2. Input of what day
+# 3. Load JSON file data.JSON
+# 4. Find the keypairs for the month and date
+import json
 
-print ("The Month is " + Input_Month)
-print ("The Day is " + Input_Day) 
+# json_string = '{"Month": "Sunsebb",, "Day": 22}'
 
-import csv
 
-with open('data.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV:
-        print(row)
+# parsed_json = json.loads(json_string)
+
+# print (parsed_json["Month"])
+
+
+with open('data.json', 'r') as fp:
+    json_data = json.load(fp)
+
+
+month = json_data["Month"]
+day = json_data["Day"]
+
+
+
+# for distro in distros_dict:
+#     print(distro['Month'])
+
+# for item in json_data:
+#     print item.get("Month").get("Day")
