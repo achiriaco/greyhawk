61c008bc0de49b1d4d220408340b66804ed72415
Angelo Chiriaco <achiriaco@hotmail.com>
2019-02-24 00:23:48 -0800

    Working Version

--- greyhawk.py
+++ greyhawk.py
@@ -13,40 +13,37 @@ import json
 # print (parsed_json["Month"])
 
 path = "data.json"
+inputmonth = "Readying"
+inputday = 5
 
 def read_json(path):
     with open(path, 'r') as fp:
         # convert string to python object
         json_data = json.load(fp)
-        # in the json dictionary we have many dicitonaries in a list
-        # iterate through the list
-        for dictionaires in json_data:
-            # for each ditionary print the keys
-            print(dictionaires.keys())
-            # for each dictionary print this key
-            print(dictionaires['Month'])
-            # here we are assignin the key month to this variable
-            # caution however as ever new iterated dictionary will overwrite the prvious entry
-            month = dictionaires["Month"]
-
-            day = dictionaires["Day"]
-
-'''i imagine youl need to do some sort of if condition here and if successful you would return the dictionary like so
-
-        if input == month:
-            return dictionaries
-
-this will return that one dictionary or json object to the client'''
-
-
-# for distro in distros_dict:
-#     print(distro['Month'])
-
-# for item in json_data:
-#     print item.get("Month").get("Day")
+        for result in json_data:
+            if result['Month'] == inputmonth:
+                if result['Day'] == inputday:
+                    Month = result["Month"]
+                    Day = result["Day"]
+                    DayOfWeek = result["DayOfWeek"]
+                    Season = result["Season"]
+                    Luna = result["Luna"]
+                    Celene = result["Celene"]
+                    Solstace = result["Solstace"]
+                    Special = result["Special"]
+                    print("The Month is: " + Month )
+                    print("The Day is: " + str(Day) )
+                    print("The Day of the Week is: " + DayOfWeek)
+                    print("The Season is: " + Season)
+                    print("The Moon Luna is: " + Luna)
+                    print("The Moon Celene is: " + Celene)
+                    if not Solstace == "":
+                        print("The Solstace is: " + Solstace)
+                    if not Special == "":
+                        print(Special)
+                                       
 
 '''if name == main is for running code when it is the main module. This code will not run if this module gets imported, it is good for keeping test code without running when importing'''
 if __name__ == '__main__':
     # execute running the file here if running from this page
     print(read_json(path))
-    
